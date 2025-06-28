ef90bb2571babf67e1802d924acfaae38ee5ec11
JAHID KHAN <jkwonderlover@gmail.com>
2025-06-28 22:47:42 +0600

    Commiting.

--- src/main/kotlin/com/jahidkhan/hello_api/HelloWorldController.kt
+++ src/main/kotlin/com/jahidkhan/hello_api/HelloWorldController.kt
@@ -2,6 +2,7 @@ package com.jahidkhan.hello_api
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import kotlin.random.Random // importing the random number machine from kotlin library
 
 // @RestController tells Spring that this class will handle web requests.
 @RestController
@@ -9,8 +10,15 @@ class HelloWorldController {
 
     // @GetMapping("/hello") tells Spring to run this function
     // when someone accesses the "/hello" URL.
-    @GetMapping("/hello")
+    @GetMapping("/number")
     fun sayHello(): String {
-        return "Hello, World! My first API is working. A different me starts here. A dream starts here."
+        val randomNum = Random.nextInt(1, 100) // takes a random number between 1 and 100
+        val message = if (randomNum % 2 == 0) { // using if-else as expressions
+            "This an even number: $randomNum"  // and checking if the random number is divisible by 2 or not
+        }
+        else {
+            "This is an odd number: $randomNum"
+        }
+        return message
     }
 }
